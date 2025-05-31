8198932fc17a955af7dfb20056eed2b4e554aa9c
hridhika <hridhika031@gmail.com>
2025-05-31 10:58:32 +0530

    deployed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Thank u and good night, this is a test of the Vite + React setup.
       </p>
     </>
   )
